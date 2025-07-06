018ccbdd40c866ac134084972f1fb85bda3d7a0f
Seunghyub choi <seunghyub.choi@gmail.com>
2025-07-06 11:08:18 +0900

    사용자 정의 리포지토리, Auditing Only JPA = @MappedSuperClass(@Pre, PostPersist, Update)

---
+++ src/test/java/study/datajpa/entity/MemberTest.java
@@ -0,0 +1,46 @@
+package study.datajpa.entity;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.repository.MemberRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+@Transactional
+@Rollback(false)
+class MemberTest {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    void JpaEventBaseEntity() throws InterruptedException {
+        // given
+        Member member = new Member("member1", 10);
+        memberRepository.save(member); // @PrePersist
+
+        Thread.sleep(100);
+        member.setUsername("member2");
+
+        em.flush();
+        em.clear();
+
+        // when
+        Member findMember = memberRepository.findById(member.getId()).get();
+
+        // then
+        System.out.println("findMember.getCreatedDate = " + findMember.getCreatedDate());
+        System.out.println("findMember.getUpdatedDate = " + findMember.getUpdatedDate());
+
+    }
+
+}
